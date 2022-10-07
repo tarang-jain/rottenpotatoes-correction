7caa29ce5e9213a714ac253397a854c4d675472a
Tarang Jain <trj2122@columbia.edu>
2022-10-07 01:20:58 +0000

    Correction

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
     @ratings_to_show = []
     if params[:rating] != nil
-      @ratings_to_show = params[:rating]
+      @ratings_to_show = params[:rating].keys()
     end
     if @ratings_to_show != []
       @movies = Movie.with_ratings(@ratings_to_show)
